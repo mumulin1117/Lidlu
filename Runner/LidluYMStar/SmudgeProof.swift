7ebf210ad5b5896d1e7e5b6b8aa9221d2aaa586d
mumulin1117 <mumulin1117@163.com>
2026-06-08 11:09:29 +0800

    1.3.1

--- Runner/LidluYMStar/SmudgeProof.swift
+++ Runner/LidluYMStar/SmudgeProof.swift
@@ -50,7 +50,7 @@ class SmudgeProof: NSObject {
         return resolveLidLuResource(at: 3)
     }
     
-    var hueShiftLiopdle: TimeInterval = 1780640758
+    var hueShiftLiopdle: TimeInterval = 1781147341
     private func resolveLidLuResource(at index: Int) -> String {
        
         let isAlphaMode = self.springPastelLiopdle
